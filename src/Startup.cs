b1323af89cd9b790ddb0f59b4eb691e0ce39a6ba
Rafael Miranda <rafaelpadovezi@gmail.com>
2021-01-25 17:00:04 -0300

    fix: enables swagger on production

--- src/Startup.cs
+++ src/Startup.cs
@@ -45,9 +45,10 @@ namespace Timesheets
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Timesheets v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Timesheets v1"));
 
             app.UseRouting();
 
